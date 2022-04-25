b5d3e1fd717ca48f3fb4da5e86fc26a3907a2481
Luciano Schultz <lucianoschultzcruz@gmail.com>
2022-04-25 19:18:13 -0300

    implementação do exercicio 3

---
+++ ex3/exercicio.js
@@ -0,0 +1,6 @@
+let nome = prompt('Qual é seu nome?')
+let salario = prompt('Qual o valor do salario do funcionario')
+
+console.log("Nome do funcionario: " + nome+"\n"+
+    "Salario: " + salario + "\n"+
+    `o funcionario ${nome} tem um salario Lucianode ${salario} em julho`);
