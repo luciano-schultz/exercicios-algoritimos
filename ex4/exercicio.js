dcb70c20da3dad5a61890c3b1593f3a4c640284e
Luciano Schultz <lucianoschultzcruz@gmail.com>
2022-04-25 20:06:48 -0300

    alteração, convertando para inteiro o prompt

--- ex4/exercicio.js
+++ ex4/exercicio.js
@@ -1,4 +1,4 @@
-let n1 = prompt('Digite um valor?')
-let n2 = prompt('Digite um valor?')
-let soma = parseInt(n1) + parseInt(n2);
+let n1 = Number(prompt('Digite um valor?'));
+let n2 = Number(prompt('Digite um valor?'));
+let soma =n1 + n2;
 console.log(`A Soma entre ${n1} e ${n2} é: ` + soma);
