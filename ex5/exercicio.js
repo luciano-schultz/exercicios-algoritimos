c69bb4a088e5ce75432d70ef2b02be01d0b2b670
Luciano Schultz <lucianoschultzcruz@gmail.com>
2022-04-25 19:57:06 -0300

    implementação do exercicio 5

---
+++ ex5/exercicio.js
@@ -0,0 +1,4 @@
+let n1 = prompt('Digite o valor da primeira nota?')
+let n2 = prompt('Digite o valor da primeira nota?')
+let media = (parseInt(n1) + parseInt(n2)) / 2;
+console.log(`A média entre entre ${n1} e ${n2} é: ` + media);
