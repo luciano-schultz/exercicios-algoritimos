685ae42157a5672edafa3dde41c023986b95b9e4
Luciano Schultz <lucianoschultzcruz@gmail.com>
2022-04-26 17:24:05 -0300

    implementação de exercicio

---
+++ ex10/exercicio.js
@@ -0,0 +1,4 @@
+let largura = Number(prompt('Digite a largura'));
+let altura = Number(prompt('Digite a altura'));
+console.log("A area ser pintada: " + (largura * altura) +
+    "m gastando " + ((largura * altura) / 2) + "L");
